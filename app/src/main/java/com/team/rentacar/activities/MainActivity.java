8fcc8b1dee33305f61aa4ef688e8c5cb94a42e4e
Hafiz Hamza <hafiz.hamza@brainplow.com>
2019-06-18 10:58:21 +0500

    push

--- app/src/main/java/com/team/rentacar/activities/MainActivity.java
+++ app/src/main/java/com/team/rentacar/activities/MainActivity.java
@@ -3,9 +3,12 @@ package com.team.rentacar.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +22,7 @@ public class MainActivity extends BaseActivity {
     TextView signOut;
     FirebaseUser currentUser;
     private DatabaseReference databaseReference;
-
+    Toolbar toolbar;
     @Override
     protected void onStart() {
         super.onStart();
@@ -30,26 +33,43 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Car Glide");
         signOut=findViewById(R.id.signout);
         mAuth = FirebaseAuth.getInstance();
-        signOut.setOnClickListener(v->{
-            if(currentUser!=null) {
 
-                SharedPreferences sharedpreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putString("token", "");
-                editor.commit();
-                mAuth.signOut();
-                logout();
-            }else
-                showErrorMessage("Invalid User");
-
-        });
     }
     private void logout() {
+        SharedPreferences sharedpreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("token", "");
+        editor.commit();
+        mAuth.signOut();
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+        // return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if(item.getItemId()==R.id.action_settings){
+            if(currentUser!=null) {
+                logout();
+            }else
+                showErrorMessage("Invalid User");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
